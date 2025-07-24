256b13fe8b684e3f0e3969fff242827cf3b75bd8
Thanh Vu Viet <125642578+ThanhVuViet@users.noreply.github.com>
2025-07-24 11:45:36 +0700

    .

--- app/src/main/java/com/example/login/login/shop_sqlite/Api/ApiClient.java
+++ app/src/main/java/com/example/login/login/shop_sqlite/Api/ApiClient.java
@@ -2,13 +2,16 @@ package com.example.login.login.shop_sqlite.Api;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
+import com.google.gson.Gson; // Import Gson
+import com.google.gson.GsonBuilder; // Import GsonBuilder
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApiClient {
-    private static Retrofit retrofit = null;
+    private static final String BASE_URL = "http://10.0.2.2:5287/";
+    private static Retrofit retrofit;
 
     public static Retrofit getClient() {
         if (retrofit == null) {
@@ -24,10 +27,14 @@ public class ApiClient {
                     .writeTimeout(30, TimeUnit.SECONDS)
                     .build();
 
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.setDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+            Gson gson = gsonBuilder.create();
+
             retrofit = new Retrofit.Builder()
-                    .baseUrl("http://10.0.2.2:5287/") // Backend URL
+                    .baseUrl(BASE_URL)
                     .client(client)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
         }
         return retrofit;
